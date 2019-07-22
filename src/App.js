2f1333c837be02ea67f308486d45f99e51139a0a
Prateek Thapa <prateekthapa26@gmail.com>
2019-07-22 16:18:23 +0545

    removed unneeded files and code

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
 import './App.css';
 
-// import DateRangeCalendar from './DateRangeCalendar';
-// import FlatpickrDatePicker from './FlatpickrDatePicker';
 import BootstrapDateRangePicker from './BootstrapDateRangePicker';
-// import BlueprintDateRangePicker from './BlueprintDateRangePicker';
 
 export default function App() {
   return (
     <div>
-      {/* <DateRangeCalendar /> */}
       <BootstrapDateRangePicker />
-      {/* <FlatpickrDatePicker /> */}
-      {/* <BlueprintDateRangePicker /> */}
     </div>
   );
 }
--- src/constants.js
+++
@@ -1,6 +0,0 @@
-export const DISPLAY_FORMAT = 'L';
-export const START_DATE = 'startDate';
-export const END_DATE = 'endDate';
-
-export const HORIZONTAL_ORIENTATION = 'horizontal';
-export const ANCHOR_LEFT = 'left';
